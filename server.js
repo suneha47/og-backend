6f741135e01907819dee00d5e45a01170f437482
suneha47 <sunehawadhawan518@gmail.com>
2026-05-03 05:47:14 -0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Error:", err));
+
+app.get("/", (req, res) => {
+  res.send("OG Accessories 47 Backend is Running");
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
