fd2992eb514cce9423c0ee9ef3bde2358a4df7e3
suneha47 <sunehawadhawan518@gmail.com>
2026-05-04 03:59:50 -0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -3,18 +3,31 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: [true, "Product name is required"],
+    trim: true,
   },
   price: {
     type: Number,
-    required: true
+    required: [true, "Price is required"],
+    min: 0,
   },
-  image: {
-    type: String
+  category: {
+    type: String,
+    required: [true, "Category is required"],
+    enum: ["Wallets","Side Bags","Watches","Perfumes","Sunglasses","T-Shirts","Pants","Shirts","Caps"],
   },
   description: {
-    type: String
-  }
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  imagePublicId: {
+    type: String,
+    default: "",
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Product", productSchema);
