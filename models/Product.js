89525f27b93f3812eac0390309013411de187af8
suneha47 <sunehawadhawan518@gmail.com>
2026-05-03 23:42:08 -0500

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String
+  },
+  description: {
+    type: String
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Product", productSchema);
