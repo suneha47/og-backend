27c79bf00616919009ecc9b2920cb02aab392816
suneha47 <sunehawadhawan518@gmail.com>
2026-05-03 23:40:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,27 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const productRoutes = require("./routes/productRoutes");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("Error:", err));
 
+// Test route
 app.get("/", (req, res) => {
   res.send("OG Accessories 47 Backend is Running");
 });
+
+// Products routes
+app.use("/products", productRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
