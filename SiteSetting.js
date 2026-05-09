21983cfd956108ddaf0e081b04fe2e5b94f5d5f9
suneha47 <sunehawadhawan518@gmail.com>
2026-05-08 22:46:34 -0500

    Delete SiteSetting.js

--- SiteSetting.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SiteSettingSchema = new mongoose.Schema({
-  key:   { type: String, required: true, unique: true },
-  value: { type: String, default: '' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('SiteSetting', SiteSettingSchema);
